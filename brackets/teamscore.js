6e79f93284f694b1a963e927d7f66a6a2aa50701
kevin-chen-15 <kevin.chen.3789@gmail.com>
2024-11-26 11:43:14 -0700

    Update teamscore.js

--- brackets/teamscore.js
+++ brackets/teamscore.js
@@ -1,4 +1,4 @@
-/*let teamScore = {
+/*var teamScore = {
   "Team 1": 8,
   "Team 2": 7,
   "Team 3": 6,
@@ -9,7 +9,7 @@
   "Team 8": 1
 }*/
 
-let testteamScore = {"Team 1": 5, "Team 2": 1, "Team 3": 8, "Team 4": 2, "Team 5": 5, "Team 6": 4, "Team 7": 3, "Team 8": 6}
+var testteamScore = {"Team 1": 5, "Team 2": 1, "Team 3": 8, "Team 4": 2, "Team 5": 5, "Team 6": 4, "Team 7": 3, "Team 8": 6}
 
 $.each(testteamScore,function(key,value){
   $('#ranking_section').append(key +": "+value+"<p>")
