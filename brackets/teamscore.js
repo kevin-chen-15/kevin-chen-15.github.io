9076ee13e52009e82f20aea094ea7a8b2b69d4a6
kevin-chen-15 <kevin.chen.3789@gmail.com>
2024-11-26 15:32:35 -0700

    Update teamscore.js
    sort function to team scores

--- brackets/teamscore.js
+++ brackets/teamscore.js
@@ -11,6 +11,8 @@
 
 var testteamScore = {"Team 1": 5, "Team 2": 1, "Team 3": 8, "Team 4": 2, "Team 5": 5, "Team 6": 4, "Team 7": 3, "Team 8": 6}
 
-$.each(testteamScore,function(key,value){
+var organizedScore = Object.entries(testteamScore).sort((a, b) => a[1] - b[1])
+
+$.each(organizedScore,function(key,value){
   $('#ranking_section').append(key +": "+value+"<p>")
 });
