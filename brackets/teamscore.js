eb8fb2605ff2142f404694f02de1dce49570c66a
kevin-chen-15 <kevin.chen.3789@gmail.com>
2024-12-06 09:19:15 -0700

    Update teamscore.js
    Updated display for organized scores

--- brackets/teamscore.js
+++ brackets/teamscore.js
@@ -13,6 +13,15 @@ var testteamScore = {"Team 1": 5, "Team 2": 1, "Team 3": 8, "Team 4": 2, "Team 5
 
 var organizedScore = Object.entries(testteamScore).sort((a, b) => b[1] - a[1])
 
+document.getElementById("score").innerHTML = Object.entries(organizedScore)[0][1][0]
+document.getElementById("score1").innerHTML = Object.entries(organizedScore)[1][1][0]
+document.getElementById("score2").innerHTML = Object.entries(organizedScore)[2][1][0]
+document.getElementById("score3").innerHTML = Object.entries(organizedScore)[3][1][0]
+document.getElementById("score4").innerHTML = Object.entries(organizedScore)[4][1][0]
+document.getElementById("score5").innerHTML = Object.entries(organizedScore)[5][1][0]
+document.getElementById("score6").innerHTML = Object.entries(organizedScore)[6][1][0]
+document.getElementById("score7").innerHTML = Object.entries(organizedScore)[7][1][0]
+
 $.each(organizedScore,function(key,value){
   $('#ranking_section').append(value[0] +": "+ value[1] + "<p>")
 });
