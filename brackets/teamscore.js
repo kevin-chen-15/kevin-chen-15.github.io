e579620f3fbff79c9ab60dd627f4623c1b1cb14b
kevin-chen-15 <kevin.chen.3789@gmail.com>
2024-12-06 11:31:18 -0700

    Update teamscore.js
    Further adjustments to bracket display

--- brackets/teamscore.js
+++ brackets/teamscore.js
@@ -22,6 +22,14 @@ document.getElementById("score5").innerHTML = Object.entries(organizedScore)[5][
 document.getElementById("score6").innerHTML = Object.entries(organizedScore)[6][1][0]
 document.getElementById("score7").innerHTML = Object.entries(organizedScore)[7][1][0]
 
+document.getElementById("winner").innerHTML = Object.entries(organizedScore)[0][1][0]
+document.getElementById("final").innerHTML = Object.entries(organizedScore)[0][1][0]
+document.getElementById("final1").innerHTML = Object.entries(organizedScore)[2][1][0]
+document.getElementById("victor").innerHTML = Object.entries(organizedScore)[0][1][0]
+document.getElementById("victor1").innerHTML = Object.entries(organizedScore)[1][1][0]
+document.getElementById("victor2").innerHTML = Object.entries(organizedScore)[2][1][0]
+document.getElementById("victor3").innerHTML = Object.entries(organizedScore)[3][1][0]
+
 $.each(organizedScore,function(key,value){
   $('#ranking_section').append(value[0] +": "+ value[1] + "<p>")
 });
