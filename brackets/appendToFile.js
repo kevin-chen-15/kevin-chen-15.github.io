a29d7c52730ea27b284688febb73d1a355962ce8
kevin-chen-15 <kevin.chen.3789@gmail.com>
2025-01-22 14:08:28 -0700

    Delete brackets/appendToFile.js

--- brackets/appendToFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs'); 
-
-function processText (){
-	fs.appendFile('textfile.txt', document.getElementById("name").innerHTML + '\n', function (err) { 
-		if (err) throw err; 
-	});
-	
-	alert("Your vote has been submitted successfully.");
-}
